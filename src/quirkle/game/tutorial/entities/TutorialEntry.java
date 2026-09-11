b6819f0dd873bcf08dd6f0d67dd07f4e84dc7d93
Navi <127903148+Navigierender@users.noreply.github.com>
2026-09-11 14:38:04 +0200

    Add TutorialEntry class and nickname strings in preperation
    Introduce a simple TutorialEntry POJO (title, description) in quirkle.game.tutorial.entities. Add a NICKNAMES section to English and German language files with nickname0..nickname9 entries (Bennet, Alex, Jarosch, multiple Kris).

---
+++ src/quirkle/game/tutorial/entities/TutorialEntry.java
@@ -0,0 +1,12 @@
+package quirkle.game.tutorial.entities;
+
+public class TutorialEntry {
+
+    public String title;
+    public String description;
+
+    public TutorialEntry(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
